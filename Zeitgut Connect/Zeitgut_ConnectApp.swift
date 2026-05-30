5acfb2401ebdf207073b2f7128af9c881d7d84ad
Jonas Vetsch <jonas@vetsch.com>
2026-05-30 17:19:00 +0200

    Initial Commit

---
+++ Zeitgut Connect/Zeitgut_ConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Zeitgut_ConnectApp.swift
+//  Zeitgut Connect
+//
+//  Created by Jonas Vetsch on 30.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct Zeitgut_ConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
